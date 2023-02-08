9e3cbebee3dcfb1f4731d4c1ab0439b9fa402dfd
Harikrishnan <harikrishnan8421@gmail.com>
2023-02-08 12:30:03 +0530

    HTML and JS training

---
+++ HTML-JS/login.js
@@ -0,0 +1,32 @@
+function test(){
+	console.log("test");
+}
+function loadRegisterPage(){
+	window.location.href="register.html";
+}
+
+function loadloginPage(){
+	window.location.href="login.html";
+}
+
+function login(){
+
+let username=document.getElementById("userName").value;
+let pwd=document.getElementById("pass").value;
+alert("Login successful");
+	
+}
+
+function register(){
+
+let username=document.getElementById("userName");
+let pwd=document.getElementById("pass");
+alert("Registration successful");
+	
+}
+
+function cancel(){
+
+document.getElementById("userName").value="";
+document.getElementById("pass").value="";
+}
